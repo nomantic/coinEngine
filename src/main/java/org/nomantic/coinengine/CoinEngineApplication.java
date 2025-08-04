df784fde9189de34ead1c0bfe1ea561effa31c44
noman <noman121998@gmail.com>
2025-08-04 13:11:50 +0200

    first commit

---
+++ src/main/java/org/nomantic/coinengine/CoinEngineApplication.java
@@ -0,0 +1,13 @@
+package org.nomantic.coinengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinEngineApplication.class, args);
+    }
+
+}
